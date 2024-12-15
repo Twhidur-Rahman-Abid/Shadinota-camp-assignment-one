11b13e438cb3a5223c216803045239cb001fcb9d
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2024-12-15 20:53:06 +0600

    Change pageName to pageTitle for toogle black-logo class

--- js/index.js
+++ js/index.js
@@ -1,34 +1,26 @@
 const mobileBtn = document.querySelector(".nav--mobile-btn");
 const mobileNav = document.querySelector(".nav--mobile");
 const logo = document.querySelector(".logo");
-const pageName = location.pathname.split("/").pop();
+const pageTitle = document.getElementsByTagName("title")[0].innerText;
 
 let previusImage;
 mobileBtn.addEventListener("click", (e) => {
   const imageName = mobileBtn.src.split("/").pop();
 
   //change icon image
-  const navfunc = () => {
-    if (imageName === "hamburger.svg" || imageName === "black-hamburger.svg") {
-      previusImage = imageName;
 
-      pageName === "index.html"
-        ? (mobileBtn.src = "./images/x.svg")
-        : (mobileBtn.src = "../images/x.svg");
-    } else {
-      pageName === "index.html"
-        ? (mobileBtn.src = `./images/${previusImage}`)
-        : (mobileBtn.src = `../images/${previusImage}`);
-    }
+  if (imageName === "hamburger.svg" || imageName === "black-hamburger.svg") {
+    previusImage = imageName;
 
-    //Class toggle
-    mobileNav.classList.toggle("nav-open");
-    if (pageName === "index.html") {
-      logo.classList.toggle("black-logo");
-    }
-  };
+    mobileBtn.src = "../images/x.svg";
+  } else {
+    mobileBtn.src = `../images/${previusImage}`;
+  }
 
-  navfunc();
-
-  console.log(imageName);
+  // Class toggle
+  mobileNav.classList.toggle("nav-open");
+  if (pageTitle === "Home") {
+    console.log("black logo");
+    logo.classList.toggle("black-logo");
+  }
 });
